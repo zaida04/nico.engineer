e8573da63ea659fbd48c17e62a9743eff8a0d94f
zaida04 <nico.03727+github@gmail.com>
2022-06-13 20:16:49 -0400

    feat: use luxon and some wording changes

--- components/Post/SmallCard.tsx
+++ components/Post/SmallCard.tsx
@@ -1,9 +1,11 @@
+import { DateTime } from "luxon";
+
 export default function SmallCard(props: { slug: string; publishedAt: string; readingTime: string; title: string; description: string }) {
     return (
         <a href={`/blog/${props.slug}`}>
             <div className="max-w-[45rem] pb-10 transition hover:scale-110">
                 <div className="flex text-gray-500">
-                    <p>{props.publishedAt}</p>
+                    <p>{DateTime.fromISO(props.publishedAt).toRelative()}</p>
                     <p className="px-2">•</p>
                     <p>{props.readingTime} </p>
                 </div>
