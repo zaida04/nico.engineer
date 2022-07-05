e6f6a1c2fc400d8ea3b1ffba51d4798837b01e3f
zaida04 <nico.03727+github@gmail.com>
2022-07-05 00:20:32 -0400

    feat: neat table

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,9 +1,10 @@
 import Statistic from "./Statistic";
 import Image from "next/image";
+import XMailTable from "./Tables/XMailTable";
 
 const MDXComponents = {
     h1: (props: { children: any }) => (
-        <h1 className="text-5xl font-bold font-rubik leading-loose" {...props}>
+        <h1 className="text-5xl font-bold font-rubik pb-8 md:pb-4" {...props}>
             {props.children}
         </h1>
     ),
@@ -15,13 +16,14 @@ const MDXComponents = {
         );
     },
     p: (props: { children: any }) => {
-        return <p className="pb-4 indent-8">{props.children}</p>;
+        return <p className="pb-8 indent-8 text-center md:text-left">{props.children}</p>;
     },
     code: (props: { children: any }) => {
         return <code className="text-red-600">{props.children}</code>;
     },
     // eslint-disable-next-line jsx-a11y/alt-text
     img: (props: { src: any }) => <img {...props} className="pt-8" />,
+    XMailTable,
 };
 
 export default MDXComponents;
---
+++ components/Tables/XMailTable.tsx
@@ -0,0 +1,28 @@
+export default function XMailTable() {
+    return (
+        <div className="pb-6">
+            <table className="table-auto border-2 border-spacing-2">
+                <thead>
+                    <tr>
+                        <th className="border border-slate-600 md:px-20">Subject</th>
+                        <th className="border border-slate-600 md:px-24">Sender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="border border-slate-700 md:px-20">AP Computer Science Homework</td>
+                        <td className="border border-slate-700 md:px-24">Mr. Guy</td>
+                    </tr>
+                    <tr>
+                        <td className="border border-slate-700 md:px-20">Materials needed</td>
+                        <td className="border border-slate-700 md:px-24">Ms. Teacher</td>
+                    </tr>
+                    <tr>
+                        <td className="border border-slate-700 md:px-20">Welcome back to school</td>
+                        <td className="border border-slate-700 md:px-24">Mr. Principal</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
