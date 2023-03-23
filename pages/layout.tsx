7651a1df44dada16107aef788872eb69072a3a08
zaida04 <nico.03727+github@gmail.com>
2023-03-23 18:25:06 -0400

    feat: go back on blog

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,10 +1,13 @@
+import { useRouter } from "next/router";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Layout(props: { children: any }) {
+    const router = useRouter();
+
     return (
         <>
-            <Navbar />
+            <Navbar goBack={router.pathname.includes("blog")} />
             <main>{props.children}</main>
             <div className="text-white flex pt-8 pl-10 pb-4">
                 <Footer />
