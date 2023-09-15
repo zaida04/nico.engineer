e89aa5dbcdf0550b354e842a5529e8431a85cbcd
zaida04 <nico.03727+github@gmail.com>
2023-09-15 00:06:22 -0400

    feat: use only this year

--- lib/wakatime.ts
+++ lib/wakatime.ts
@@ -1,9 +1,12 @@
-export const getTimeCoding = () =>
-    fetch(`https://wakatime.com/api/v1/users/current/summaries?start=2023-01-01&api_key=${process.env.WAKATIME_API_KEY}`, {
+export const getTimeCoding = async () => {
+    const req = await fetch(`https://wakatime.com/api/v1/users/current/stats/last_year?api_key=${process.env.WAKATIME_API_KEY}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
         },
-    })
-        .then((x) => x.json())
-        .then((data) => data?.data?.[0]?.grand_total?.digital);
+    });
+
+    const data = await req.json();
+    const total = data?.data?.human_readable_total;
+    return total;
+}
