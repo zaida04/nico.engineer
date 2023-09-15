07a566531bc60cdf28eded7d65065d4ad2785afc
Nico <nico.03727+github@gmail.com>
2023-09-15 00:00:28 -0400

    Update wakatime.ts

--- lib/wakatime.ts
+++ lib/wakatime.ts
@@ -1,9 +1,9 @@
 export const getTimeCoding = () =>
-    fetch(`https://wakatime.com/api/v1/users/current/all_time_since_today?api_key=${process.env.WAKATIME_API_KEY}`, {
+    fetch(`https://wakatime.com/api/v1/users/current/summaries?start=2023-01-01&api_key=${process.env.WAKATIME_API_KEY}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
         },
     })
         .then((x) => x.json())
-        .then((data) => data?.data?.text);
+        .then((data) => data?.data?.[0]?.grand_total?.digital);
